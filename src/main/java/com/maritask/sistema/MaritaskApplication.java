b0cdd45970747862de78b9a6579f354df886639d
Akio Tanaka <54818331+akiotnk00@users.noreply.github.com>
2024-08-04 14:22:35 -0300

    Spring Adicionado

---
+++ src/main/java/com/maritask/sistema/MaritaskApplication.java
@@ -0,0 +1,13 @@
+package com.maritask.sistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaritaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaritaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maritask/sistema/MaritaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maritask.sistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaritaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
